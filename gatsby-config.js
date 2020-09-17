c4199f5fdb31c0fb14c9b5444bc4c77824a0c869
Niall Lennon <niall@zenhub.com>
2020-09-16 20:39:56 -0700

    delete cache and public

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,34 +9,46 @@ const path = require("path");
 module.exports = {
   pathPrefix: "/nialllennon.com",
   siteMetadata: {
-    title: `Niall Lennon | Software Developer`
+    title: `Niall Lennon | Software Developer`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `reenie beanie`,
-          `raleway:200,600` // you can also specify font weights and styles
-        ],
-        display: "swap"
-      }
+        fonts: [`reenie beanie`, `raleway:200,600`, `open sans`],
+        display: "swap",
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: path.join(__dirname, `src`, `images`)
-      }
+        path: path.join(__dirname, `src`, `images`),
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-153520601-1"
-      }
+        trackingId: "UA-153520601-1",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        allExtensions: true, // defaults to false
+      },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+  ],
 };
